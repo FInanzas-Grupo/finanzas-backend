9bf516f1cdbd2a5448cbee701d69a5871ad4b4a3
Renzo <renzor274@gmail.com>
2026-06-29 23:13:37 -0500

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,17 @@
 import { Sequelize } from "sequelize";
 import { env } from "./env.js";
 
+const isLocal = env.databaseUrl.includes("localhost") || env.databaseUrl.includes("127.0.0.1");
+
 export const sequelize = new Sequelize(env.databaseUrl, {
   dialect: "postgres",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  },
+  dialectOptions: isLocal
+    ? {}
+    : {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      },
   logging: false
 });
